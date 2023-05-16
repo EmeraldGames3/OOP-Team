e26853dd02b6b7bd6f039ab052bc78c60c771c2f
Bogdan <bobo.oprisiu@gmail.com>
2023-05-16 17:50:57 +0300

    Add comments

--- Project/Controller/ElectricScooterController/ElectricScooterController.cpp
+++ Project/Controller/ElectricScooterController/ElectricScooterController.cpp
@@ -5,10 +5,12 @@
 using namespace Controller;
 using namespace Domain;
 
+///Constructor with default parameters
 ElectricScooterController::ElectricScooterController(shared_ptr<ElectricScooterRepository> repo) {
-    repository = std::move(repo);
+    repository = repo;
 }
 
+///Add a new scooter to the data Base
 void ElectricScooterController::add(
         string id, string model, const string &date, float mileage, string location, string condition
 ) {
@@ -17,6 +19,8 @@ void ElectricScooterController::add(
     repository->add(scooter);
 }
 
+///Remove a scooter from the Data Base
+///@throws invalid_argument if the id of the scooter to be removed does not exist
 void ElectricScooterController::remove(const string &id) {
     if (!find(id))
         throw invalid_argument("");
@@ -24,10 +28,12 @@ void ElectricScooterController::remove(const string &id) {
     repository->remove(scooter);
 }
 
+///Get all scooters in the Data Base
 vector<ElectricScooter> ElectricScooterController::getAll() {
     return repository->getAll();
 }
 
+///Find if a scooter exists in the Data Base
 bool ElectricScooterController::find(const string &id) {
     vector<ElectricScooter> scooters = getAll();
     for (auto &scooter: scooters)
@@ -36,42 +42,55 @@ bool ElectricScooterController::find(const string &id) {
     return false;
 }
 
+///Update the model of a scooter
+///@throws invalid_argument if the scooter id does not exist
 void ElectricScooterController::updateModel(const string &model, const string &id) {
     if (!find(id))
         throw invalid_argument("");
     repository->updateModel(model, id);
 }
 
+///Update the mileage of a scooter
+///@throws invalid_argument if the scooter id does not exist
 void ElectricScooterController::updateMileage(float mileage, const string &id) {
     if (!find(id))
         throw invalid_argument("");
     repository->updateMileage(mileage, id);
 }
 
+///Update the id of a scooter
+///@throws invalid_argument if the scooter id does not exist
 void ElectricScooterController::updateID(const string &id, const string &oldId) {
     if (!find(oldId))
         throw invalid_argument("");
     repository->updateID(id, oldId);
 }
 
+///Update the last location of scooter
+///@throws invalid_argument if the scooter id does not exist
 void ElectricScooterController::updateLocation(const string &location, const string &id) {
     if (!find(id))
         throw invalid_argument("");
     repository->updateLocation(location, id);
 }
 
+///Update the condition of a scooter
+///@throws invalid_argument if the scooter id does not exist
 void ElectricScooterController::updateCondition(const string &condition, const string &id) {
     if (!find(id))
         throw invalid_argument("");
     repository->updateCondition(condition, id);
 }
 
+///Update the commissioning Date of a scooter
+///@throws invalid_argument if the scooter id does not exist
 void ElectricScooterController::updateDate(const string &date, const string &id) {
     if (!find(id))
         throw invalid_argument("");
     repository->updateDate(date, id);
 }
 
+///Filter all scooters by data
 vector<ElectricScooter> ElectricScooterController::ageFiltered(Date value) {
     vector<ElectricScooter> scooters = getAll();
     vector<ElectricScooter> filteredScooters;
@@ -87,6 +106,7 @@ vector<ElectricScooter> ElectricScooterController::ageFiltered(Date value) {
     return filteredScooters;
 }
 
+///Get all scooters sorted by date
 vector<ElectricScooter> ElectricScooterController::ageSorted() {
     vector<ElectricScooter> scooters = getAll();
     sort(scooters.begin(), scooters.end(), [](ElectricScooter a, ElectricScooter b) {
@@ -95,6 +115,7 @@ vector<ElectricScooter> ElectricScooterController::ageSorted() {
     return scooters;
 }
 
+///Filter all scooters based on mileage
 vector<ElectricScooter> ElectricScooterController::mileageFiltered(float value) {
     vector<ElectricScooter> scooters = getAll();
     vector<ElectricScooter> filteredScooters;
@@ -110,6 +131,7 @@ vector<ElectricScooter> ElectricScooterController::mileageFiltered(float value) 
     return filteredScooters;
 }
 
+///Search for a string in the last location of a scooter
 vector<ElectricScooter> ElectricScooterController::lastLocationSearch(const string &location) {
     vector<ElectricScooter> scooters = getAll();
     vector<ElectricScooter> matchingScooters;
@@ -120,6 +142,5 @@ vector<ElectricScooter> ElectricScooterController::lastLocationSearch(const stri
             matchingScooters.push_back(scooter);
         }
     }
-
     return matchingScooters;
 }
